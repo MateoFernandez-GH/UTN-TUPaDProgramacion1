7838654a948f3f39388eb4b6cbc350131de526fa
MateoFernandez-GH <myapps.mateo@gmail.com>
2026-06-01 15:55:27 -0300

    Avances en funciones de carga inicial de productos y muestra de inventarios.

--- Parcial 2 - Sistema de Control de Inventarios.py
+++ Parcial 2 - Sistema de Control de Inventarios.py
@@ -1,18 +1,46 @@
 # Funciones auxiliares
 
-def normalizar_nombre():
-    pass
+def normalizar_nombre(nombre):
+    return nombre.strip().lower() 
 
 def buscar_herramienta():
     pass
 
 # Funciones relativas a las opciones del menú
 
-def carga_inicial():
-    pass
+def carga_inicial(inventario):
+    print("\n==============================================")
+    print("Carga de herramientas iniciales")
 
-def mostrar_inventario():
-    pass
+    try:
+        cantidad = int(input("Ingrese la cantidad de herramientas a cargar: "))
+        if cantidad < 0:
+            raise ValueError("La cantidad no puede ser negativa.")
+    except ValueError as e:
+        print(f"Error: {e}")
+        return
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return
+
+    for _ in range(cantidad):
+        nombre = normalizar_nombre(input("Ingrese el nombre de la herramienta: "))
+        try:
+            stock = int(input("Ingrese el stock inicial: "))
+            if stock < 0:
+                raise ValueError("El stock inicial no puede ser negativo.")
+        except ValueError as e:
+            print(f"Error: {e}")
+            return
+        inventario.append({"herramienta": nombre, "cantidad": stock})
+
+
+def mostrar_inventario(inventario):
+    print("\n==============================================")
+    print("Inventario:")
+    for herramienta in inventario:
+        print(f"- {herramienta['herramienta']}: {herramienta['cantidad']} unidades\n")
+
 
 def consultar_stock():
     pass
@@ -20,15 +48,23 @@ def consultar_stock():
 # Funciones estructurales del Menú  
 
 def mostrar_menu():
-    pass
+    print("\n==============================================")
+    print("Menú de opciones:")
+    print("1. Carga de herramientas iniciales")
+    print("2. Visualización de inventario")
+    print("3. Consulta de stock")
+    print("4. Reporte de agotados")
+    print("5. Alta de nuevo producto")
+    print("6. Actualizacion de stock (venta / ingreso)")
+    print("7. Salir\n")
 
 def ejecutar_menu(opcion, inventario):
     
     match opcion :
         case "1":
-            carga_inicial()
+            carga_inicial(inventario)
         case "2":
-            mostrar_inventario()
+            mostrar_inventario(inventario)
         case "3":
             consultar_stock()
         case "4":
@@ -36,7 +72,7 @@ def ejecutar_menu(opcion, inventario):
         case "5":
             pass # Alta de nuevo producto 
         case "6":
-            pass # Modificación de stock - venta o ingreso
+            pass # Actualización de stock - venta o ingreso
         case "7":
             print("Saliendo del programa. ¡Hasta luego!")
             # Retorna falso para indicar que el programa debe finalizar.
